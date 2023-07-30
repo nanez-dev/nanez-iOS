950fa6ee6f20e04527951af95fcbb109a438ccfb
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-07-30 22:10:53 +0900

    [fix] Color+ 헥사코드 스트링으로 가능하게 변경

--- Na'nez/Utils/Colors.swift
+++ Na'nez/Utils/Colors.swift
@@ -8,13 +8,22 @@
 import Foundation
 import UIKit
 extension UIColor {
-    convenience init(hex: UInt, alpha: CGFloat = 1.0) {
-        self.init(
-            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(hex & 0x0000FF) / 255.0,
-            alpha: CGFloat(alpha)
-        )
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let alpha, red, green, blue: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (alpha, red, green, blue) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (alpha, red, green, blue) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (alpha, red, green, blue) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (alpha, red, green, blue) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: CGFloat(alpha) / 255)
     }
     class var mainGray: UIColor { UIColor(named: "mainGray") ?? UIColor() } //주로 버튼에쓰이는 그레이
     class var mainturquoise: UIColor { UIColor(named: "mainturquoise") ?? UIColor() } // 버튼에 주로쓰이는 청록 
@@ -35,13 +44,5 @@ extension UIColor {
                )
            }
         
-        convenience init(argb: Int) {
-            self.init(
-                red: (argb >> 16) & 0xFF,
-                green: (argb >> 8) & 0xFF,
-                blue: argb & 0xFF,
-                a: (argb >> 24) & 0xFF
-            )
-        }
 }
 

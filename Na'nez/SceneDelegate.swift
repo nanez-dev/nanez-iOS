1643876c04643d7bed696004eb5e427b1fbb053a
mcrkgus <mcrkgus@naver.com>
2023-08-19 20:29:12 +0900

    [feat] LoginView - API 연동 완료

--- Na'nez/SceneDelegate.swift
+++ Na'nez/SceneDelegate.swift
@@ -18,7 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
           window = UIWindow(windowScene: windowScene) // SceneDelegate의 프로퍼티에 설정
-          let HomeVC = TabController() // 맨 처음 보여줄 TabController
+          //let HomeVC = TabController() // 맨 처음 보여줄 TabController
+        let HomeVC = LoginViewController()
 
           window?.rootViewController = HomeVC
           window?.makeKeyAndVisible()

0a48e43d32c01d270cced9f7400ea37b550cc5ce
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-10-23 01:45:02 +0900

    feat : Home CardView

--- Na'nez/Presents/MyInfoUI/MyInfoViewController.swift
+++ Na'nez/Presents/MyInfoUI/MyInfoViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class MyInfoViewController: BaseViewController {
     private let navibar:CustomNaviBar = CustomNaviBar(frame: .zero)
+    private let myinfoView:MyInfoView = MyInfoView(frame: .zero)
     
     override func layout() {
         navibar.snp.makeConstraints{
@@ -16,9 +17,14 @@ class MyInfoViewController: BaseViewController {
             $0.width.equalToSuperview().offset(0)
             $0.height.equalTo(52)
         }
+        myinfoView.snp.makeConstraints {
+            $0.top.equalTo(navibar.snp.bottom).offset(0)
+            $0.leading.bottom.trailing.equalToSuperview()
+        }
     }
     override func configure() {
         self.view.addSubview(navibar)
+        self.view.addSubview(myinfoView)
         self.navibar.backBtn.isHidden = true
         self.navibar.searchBtn.isHidden = true
         self.navibar.navititleLabel.text = "마이페이지"

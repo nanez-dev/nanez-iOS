605fe78cc4ae83e1a474c2d7d17b441c7e9bc932
emptyhead <korea6370@yu.ac.kr>
2023-12-01 12:42:59 +0900

    feature : 이메일 인증 페이지(80%)

---
+++ Na'nez/Data/Repository/SelectEmailRepository.swift
@@ -0,0 +1,8 @@
+//
+//  SelectEmailRepository.swift
+//  Na'nez
+//
+//  Created by KIM Hyung Jun on 11/27/23.
+//
+
+import Foundation
---
+++ Na'nez/Presents/CustomAlertUI/CustomAlertView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomAlertView.swift
+//  Na'nez
+//
+//  Created by KIM Hyung Jun on 11/28/23.
+//
+
+import Foundation

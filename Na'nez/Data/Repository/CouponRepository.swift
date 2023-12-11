a949a67b8e0fdb7ad02dd455f4e552d391e9f158
emptyhead <korea6370@yu.ac.kr>
2023-12-11 13:05:05 +0900

    add : 쿠폰 페이지 화면 제작

---
+++ Na'nez/Data/Repository/CouponRepository.swift
@@ -0,0 +1,8 @@
+//
+//  CouponRepository.swift
+//  Na'nez
+//
+//  Created by KIM Hyung Jun on 12/11/23.
+//
+
+import Foundation
---
+++ Na'nez/Domain/UseCase/CouponUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  CouponUseCase.swift
+//  Na'nez
+//
+//  Created by KIM Hyung Jun on 12/11/23.
+//
+
+import Foundation
